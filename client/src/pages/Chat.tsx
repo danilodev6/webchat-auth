41c8d51ffaa76c3bd2085c87bdad87567b3dbd95
Danilo <danilo.dev6@gmail.com>
2025-10-23 12:32:40 -0300

    .

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -77,6 +77,7 @@ export default function Chat({ user, onLogout }: ChatPageProps) {
     await logout();
     socket?.close();
     onLogout();
+    console.log("Logout successful!");
   };
 
   return (
